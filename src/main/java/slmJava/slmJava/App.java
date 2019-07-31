186ca034e21fa8e138e9db6ec8990edd0280ef6d
ts <ts@tsPC>
2019-07-31 20:54:25 +0530

    base java maven code

---
+++ src/main/java/slmJava/slmJava/App.java
@@ -0,0 +1,13 @@
+package slmJava.slmJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
